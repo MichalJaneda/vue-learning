008f48291f7ce7ad9c6b1c283209d91f3b6937a5
Michał <michal.janeda@netguru.co>
2019-03-16 18:54:16 +0100

    Adding todo if it is not ''

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,17 @@
 <template>
-  <ul v-if="todos.length">
-    <li v-for="todo in todos">
-      {{ todo.text }}
-      <small>{{ todo.created_at.toLocaleString() }}</small>
-    </li>
-  </ul>
-  <h4 v-else>No Todos</h4>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input v-model="next_todo">
+      <button>Add TODO</button>
+    </form>
+    <ol v-if="todos.length">
+      <li v-for="todo in todos">
+        <strong>{{ todo.text }}</strong>
+        <small>{{ todo.created_at.toLocaleString() }}</small>
+      </li>
+    </ol>
+    <h4 v-else>No Todos</h4>
+  </div>
 </template>
 
 <script>
@@ -17,7 +23,16 @@ export default {
           text: 'Todo',
           created_at: new Date()
         }
-      ]
+      ],
+      next_todo: ''
+    }
+  },
+  methods: {
+    addTodo: function () {
+      if (this.next_todo) {
+        this.todos.push({ text: this.next_todo, created_at: new Date() })
+        this.next_todo = ''
+      }
     }
   }
 }
