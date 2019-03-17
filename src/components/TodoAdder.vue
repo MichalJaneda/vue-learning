9933f3be38279e6561ba2584cd797e7537983b8c
Michał <michal.janeda@netguru.co>
2019-03-17 14:36:54 +0100

    Move adding todos to separate component

---
+++ src/components/TodoAdder.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="next_todo">
+    <button>Add TODO</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      next_todo: '',
+      id: 1
+    }
+  },
+  methods: {
+    addTodo: function () {
+      if (this.next_todo) {
+        this.$emit('addTodo', { text: this.next_todo, id: this.id++ })
+        this.next_todo = ''
+      }
+    }
+  }
+}
+</script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <p>
-      Todos: {{ finishedTodosCount }}/{{ todos.length}}
-    </p>
-    <form @submit.prevent="addTodo">
-      <input v-model="next_todo">
-      <button>Add TODO</button>
-    </form>
-    <p v-if="todos.length">Last added TODO: {{ lastAdded }}</p>
+    <p>Todos: {{ finishedTodosCount }}/{{ todos.length}}</p>
+    <TodoAdder @addTodo="todos.push($event)"></TodoAdder>
+    <p v-if="todos.length">Last added TODO: {{ lastAdded.text }}</p>
     <ol v-if="todos.length">
       <TodoItem
         v-for="todo in todos"
-        :text="todo"
-        :key="todo"
+        :text="todo.text"
+        :key="todo.id"
         @finish="finishedTodosCount += 1">
       </TodoItem>
     </ol>
@@ -23,31 +18,22 @@
 <script>
 import { last } from 'lodash'
 import TodoItem from './TodoItem'
+import TodoAdder from './TodoAdder'
 
 export default {
   components: {
-    TodoItem
+    TodoItem,
+    TodoAdder
   },
   data: function () {
     return {
-      todos: [
-        'TODO'
-      ],
-      next_todo: '',
+      todos: [],
       finishedTodosCount: 0
     }
   },
   computed: {
     lastAdded: function () {
       return last(this.todos)
-    }
-  },
-  methods: {
-    addTodo: function () {
-      if (this.next_todo) {
-        this.todos.push(this.next_todo)
-        this.next_todo = ''
-      }
     }
   }
 }
